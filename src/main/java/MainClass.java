2d377fcd2ff2f4fcfaab9de5ba657896f2fcf37f
vimal <contacttovimal@gamail.com>
2022-04-30 14:57:12 +0800

    CodingTest

---
+++ src/main/java/MainClass.java
@@ -0,0 +1,10 @@
+public class MainClass {
+
+    public static void main(String[] args) {
+//    System.setProperty("spring.config.name","test-server");
+        System.out.println("test class");
+
+
+    }
+
+}
---
+++ src/test/java/TestClass.java
@@ -0,0 +1,15 @@
+import org.junit.Before;
+import org.junit.Test;
+
+public class TestClass {
+
+    @Before
+    public void setup(){
+
+    }
+
+    @Test
+    public void testCase1(){
+        System.out.println("test done");
+    }
+}
