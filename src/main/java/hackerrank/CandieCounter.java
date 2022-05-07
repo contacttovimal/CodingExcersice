8970e505b3dc05b997542d2169e6663d79d62156
vimal <contacttovimal@gamail.com>
2022-05-07 15:36:02 +0800

    CodingTest

---
+++ src/main/java/hackerrank/CandieCounter.java
@@ -0,0 +1,73 @@
+package hackerrank;
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class ResultCandie {
+
+    /*
+     * Complete the 'candies' function below.
+     *
+     * The function is expected to return a LONG_INTEGER.
+     * The function accepts following parameters:
+     *  1. INTEGER n
+     *  2. INTEGER_ARRAY arr
+     */
+
+    public static long candies(int n, List<Integer> arr) {
+        // Write your code here
+        int candiesCounter=0;
+        int prevScore=arr.get(0);
+        int totalCandies=0;
+        for(int i=0; i<arr.size();i++){
+            if(prevScore > arr.get(i) ){
+                candiesCounter=1;
+            }else{
+                candiesCounter++;
+            }
+            System.out.println("score :" + arr.get(i) + " , candiesCounter : " + candiesCounter + ", prevScore :" + prevScore);
+            totalCandies += candiesCounter;
+            prevScore = arr.get(i);
+        }
+        return totalCandies;
+    }
+
+}
+
+public class CandieCounter {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter( "C:\\Users\\conta\\IdeaProjects\\CodingTest\\src\\main\\resources\\test.txt" ));
+
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+        List<Integer> arr = IntStream.range(0, n).mapToObj(i -> {
+                    try {
+                        return bufferedReader.readLine().replaceAll("\\s+$", "");
+                    } catch (IOException ex) {
+                        throw new RuntimeException(ex);
+                    }
+                })
+                .map(String::trim)
+                .map(Integer::parseInt)
+                .collect(toList());
+
+        long result = ResultCandie.candies(n, arr);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
+
---
+++ src/main/java/hackerrank/FindSubStringBetweenTwoString.java
@@ -0,0 +1,69 @@
+package hackerrank;
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class ResultSubStr {
+
+    /*
+     * Complete the 'twoStrings' function below.
+     *
+     * The function is expected to return a STRING.
+     * The function accepts following parameters:
+     *  1. STRING s1
+     *  2. STRING s2
+     */
+
+    public static String twoStrings(String s1, String s2) {
+        // Write your code here
+        if(s1 == null || s2 == null){
+            return "NO";
+        }
+
+        String allcharString = "abcdefghijklmnopqrstuvwxyz";
+
+        for(int i=0;  i < allcharString.length();i++){
+            if(s1.indexOf(allcharString.charAt(i))!=-1 && s2.indexOf(allcharString.charAt(i))!=-1){
+                return "YES";
+            }
+        }
+        return "NO";
+    }
+
+}
+
+public class FindSubStringBetweenTwoString {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int q = Integer.parseInt(bufferedReader.readLine().trim());
+
+        IntStream.range(0, q).forEach(qItr -> {
+            try {
+                String s1 = bufferedReader.readLine();
+
+                String s2 = bufferedReader.readLine();
+
+                String result = ResultSubStr.twoStrings(s1, s2);
+
+                bufferedWriter.write(result);
+                bufferedWriter.newLine();
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+        });
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
